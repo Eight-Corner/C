af603f85bb6d19b0397c25070633ab51316ee480
Eight-Corner <corner3499@gmail.com>
2020-10-28 10:20:57 +0900

    2차원 배열을 이용

---
+++ C-Day05-1/C-Day05-1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-Day05-1
+//
+//  Created by 지금인 이유 on 2020/10/28.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
