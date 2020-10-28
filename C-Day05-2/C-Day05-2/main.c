2c09c32f4b421ad5c1e05a8b2a782c4ade036286
Eight-Corner <corner3499@gmail.com>
2020-10-28 11:38:21 +0900

    포인터 연산

--- C-Day05-2/C-Day05-2/main.c
+++ C-Day05-2/C-Day05-2/main.c
@@ -9,15 +9,19 @@ int main(int argc, const char * argv[]) {
     int *ptr; // * 포인터 변수라는 의미 (주소를 담을 뜻)
     
     // 포인터 변수에 배열 대입
-    ptr=a; // ptr = &a[0] || a의 0열에 주소를 ptr에 대입
+    ptr=&a[2]; // ptr = &a[0] || a의 0열에 주소를 ptr에 대입
+    // 10출력, 40출력
     
-    ptr = ptr+1; //
+    
+    ptr = ptr-2; // 10
     printf("주소 : %d \n", ptr); //a=0이였다가 1이 증가해 1번 인덱스
+    printf("주소 : %d \n", &a[0]);
     // 포인터 값을 증가
-    ptr++; // ptr = ptr+1
-    printf("ptr++ : %d \n", *ptr); //1이 한번 더 증가해서 2번 인덱스
+//    ptr++; // ptr = ptr+1
+    printf("*ptr 값은  : %d \n", *ptr); //1이 한번 더 증가해서 2번 인덱스
     // 포인터 변수의 주소를 찾아 그 안에 들어있는 값을 선택,
-    
+    ptr = ptr+3; // 40
+    printf("*ptr 값은  : %d \n", *ptr); //1이 한번 더 증가해서 2번 인덱스
     printf("\n=============\n");
     
     
---
+++ C-Day05-3/C-Day05-3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-Day05-3
+//
+//  Created by 지금인 이유 on 2020/10/28.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
