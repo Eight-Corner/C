60a59fdec1db3d3607c0bf1de4c1b68defb957ab
Eight-Corner <corner3499@gmail.com>
2020-10-28 11:15:17 +0900

    포인터 연산

--- C-Day05-2/C-Day05-2/main.c
+++ C-Day05-2/C-Day05-2/main.c
@@ -1,14 +1,14 @@
-//
-//  main.c
-//  C-Day05-2
-//
-//  Created by 지금인 이유 on 2020/10/28.
-//
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+    int int_arr[5] = {1,2,3,4,5};
+    int i;
+    
+    int *p_int_arr[5];
+    p_int_arr = int_arr;
+    printf("\n=============\n");
+    //포인트 연산
+    
 }
