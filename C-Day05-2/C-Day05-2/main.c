38a5b6ffe9b37dd2fbf21bbb509659270e4a9747
Eight-Corner <corner3499@gmail.com>
2020-10-28 11:28:48 +0900

    포인터 연산

--- C-Day05-2/C-Day05-2/main.c
+++ C-Day05-2/C-Day05-2/main.c
@@ -3,12 +3,22 @@
 
 int main(int argc, const char * argv[]) {
     
-    int int_arr[5] = {1,2,3,4,5};
-    int i;
+    // 배열 선언
+    int a[5] = {10,20,30,40,50};
+    //포인터 변수의 선언
+    int *ptr; // * 포인터 변수라는 의미 (주소를 담을 뜻)
     
-    int *p_int_arr[5];
-    p_int_arr = int_arr;
+    // 포인터 변수에 배열 대입
+    ptr=a; // ptr = &a[0] || a의 0열에 주소를 ptr에 대입
+    
+    ptr = ptr+1; //
+    printf("주소 : %d \n", ptr); //a=0이였다가 1이 증가해 1번 인덱스
+    // 포인터 값을 증가
+    ptr++; // ptr = ptr+1
+    printf("ptr++ : %d \n", *ptr); //1이 한번 더 증가해서 2번 인덱스
+    // 포인터 변수의 주소를 찾아 그 안에 들어있는 값을 선택,
+    
     printf("\n=============\n");
-    //포인트 연산
+    
     
 }
